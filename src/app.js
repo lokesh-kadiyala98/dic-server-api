a79b262a767100dbd487f005d37c832bec774a95
lokesh-kadiyala98 <33837730+lokesh-kadiyala98@users.noreply.github.com>
2019-11-01 12:23:21 +0530

    Added File: Public

--- src/app.js
+++ src/app.js
@@ -1,86 +1,60 @@
 const express = require('express')
-const mysql = require('mysql')
 const bodyParser = require('body-parser')
+const path = require('path')
 const cors = require('cors')
 const multer = require('multer')
 const app = express()
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient;
 
 app.use(cors())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 const PORT = process.env.PORT || 5000;
+const DBurl = 'mongodb+srv://root:root@dic-gkoak.mongodb.net/test?retryWrites=true&w=majority';
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Jyothi123',
-    database: 'dic_app'
-});
+app.use('/upload_file', (req, res) => {
+    
+    const storage = multer.diskStorage({
+        destination: function(req, file, cb) {
+            cb(null, 'public')
+        },
+        filename: function(req, file, cb) {
+            var ext = path.extname(file.originalname)
+            if(!file.originalname)
+                res.send({ error: 'File Upload: Something Went Wrong !!'})
+            cb(null, req.query.imgID + ext)
+        }
+    })
+    const upload = multer({ storage: storage }).single('image')
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'public')
-    },
-    filename: function(req, file, cb) {
-        cb(null, req.query.name + '-' + file.originalname)
-    }
+    upload(req, res, (err) => {
+        if (err)
+            res.send({ error: 'File Upload: Something Went Wrong !!' })
+        else if (!req.file)
+            res.send({ error: 'File Upload: Image File is Required !!' })
+        else
+            res.send({ success: 'File Upload: Success !!' })
+    })
+
 })
-const upload = multer({ storage: storage }).single('image')
 
 app.use('/submit_profile', (req, res) => {
 
-    var fileSuccess = false
-    var querySuccess = false
-
-    if(req.query.name) {
-        upload(req, res, function (err) {
-            if (err instanceof multer.MulterError) {
-                fileSuccess = false
-            } else if (err) {
-                fileSuccess = false
+    if(req.body.name) {
+        MongoClient.connect(DBurl, (err, client) => {
+            if(err)
+                res.send({ error: 'Database Connection: Seems like something went wrong!!' })
+            else {
+                const db = client.db('dic-app-database')
+                db.collection('users_form_data').insertOne(req.body)
+                client.close()
+                res.send({ success: 'Form Data: Success!!' })
             }
-            fileSuccess = true
         })
-
-        const query = 'INSERT INTO trainee_profile(name, id_numb, father_husband_name, mother_name, guardian_name, address, grama, grama_panchayat, ' + 
-        'taluk, district, mobile, email, alternative_phone, alternative_mobile, age, education_qualification, religion, gender, category, yearly_income) ' + 
-        'VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
-
-        connection.query(query, [req.query.name, 
-            req.query.individualIdentityNumber, 
-            req.query.fatherOrHusbandName, 
-            req.query.mother, 
-            req.query.guardianName,
-            req.query.address,
-            req.query.grama,
-            req.query.gramaPanchayat,
-            req.query.taluk,
-            req.query.district,
-            req.query.mobile,
-            req.query.emailId,
-            req.query.phone,
-            req.query.alternativeMobile,
-            req.query.age,
-            req.query.educationQualification,
-            req.query.religion,
-            req.query.gender,
-            req.query.category,
-            req.query.yearlyIncome
-        ], (err, rows, fields) => {
-            if(err){
-                res.json({ success: false })
-                res.sendStatus(500)
-                return
-            }
-            res.json({
-                file: fileSuccess,
-                query: true,
-            })
-        })
-        
     } else {
-        res.json({ error: 'API requires form values and file to be uploaded. None given.' })
+        res.json({ error: 'Form Values Required !! None given !!' })
     }
 
 })
